91aab56475d2246d1a8cbdb08a0ce022c0141960
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-10-27 09:42:09 +0100

    mancavano i ";"

--- main.js
+++ main.js
@@ -1,43 +1,35 @@
 // Dovete quindi analizzare la struttura e il css utilizzati e predisposti nella repo per integrare la vostra logica nel file js. Al click sull'icona dell'hamburger menu, deve essere visualizzato il menu mobile e al click sull'icona "X" il menu mobile deve scomparire.
 
-// // first method
-// // clicco sull'hamburger per far comparire il menu
-// $('.fa-bars').click(function() {
-//     $('.hamburger-menu').show()
-// });
-// //
-// // //clicco sull'icona X per fare l'effetto hide
-// $('.fa-times').click(function() {
-//     $('.hamburger-menu').hide()
-// });
+// first method
+// clicco sull'hamburger per far comparire il menu
+$('.fa-bars').click(function() {
+    $('.hamburger-menu').show();
+});
+//
+// //clicco sull'icona X per fare l'effetto hide
+$('.fa-times').click(function() {
+    $('.hamburger-menu').hide();
+});
 
 //second method using the class .active
 // $('.fa-bars').click(function() {
-//     $('.hamburger-menu').addClass('active')
+//     $('.hamburger-menu').addClass('active');
 // });
 // //
 // // //clicco sull'icona X per fare l'effetto hide
 // $('.fa-times').click(function() {
-//     $('.hamburger-menu').removeClass('active')
+//     $('.hamburger-menu').removeClass('active');
 // });
 
 //third method JS plain
-document.addEventListener("click", addActive);
-
-function addActive() {
-    var addClass = document.getElementsByClassName("hamburger-menu");
-    for(var i = 0; i < addClass.length; i++)
-    {
-        addClass[i].classList.toggle("active");
-    }
-}
-
-// document.addEventListener("click", removeActive);
+// document.getElementsByClassName("fa-bars").addEventListener("click", addActive);
+// document.addEventListener("click", addActive);
 //
-// function removeActive() {
-//     var removeClass = document.getElementsByClassName("hamburger-menu active");
-//     for(var i = 0; i < removeClass.length; i++)
+// function addActive() {
+//     var addClass = document.getElementsByClassName("hamburger-menu");
+//     for(var i = 0; i < addClass.length; i++)
 //     {
-//         removeClass[i].classList.remove('active');
+//         //aggiunge classe active se non è present e rimuove se è presente
+//         addClass[i].classList.toggle("active");
 //     }
 // }
